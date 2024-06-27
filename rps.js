2ed967b1f9fbc98a7d48a58a4f4c2b5343327252
Alex Reilly <iamalexreilly@gmail.com>
2024-06-27 12:27:57 -0400

    Create the human and computer choice functions
    Create functions to pull the user input and get the computer input
    for the rock paper scissors game, as the player will be playing against
    the computer. Also, define functions for the player and computer score
    to later determine who wins.

--- rps.js
+++ rps.js
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+function computerChoice(){
+    //get the computer's choice from random generation
+    let random = Math.floor(Math.random()*3);
+    switch (random){
+        case 0:
+            return "rock";
+        case 1: 
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+function getHumanChoice(){
+    // get the human input
+    let choice;
+    choice = prompt("Rock, Paper, or Scissors?: ");
+    while (true){
+        if(choice === "rock" || choice === "paper" || choice === "scissors"){
+            return choice;
+        }else{
+            choice = prompt("Try again. Rock, Paper, or Scissors?: ");
+        }
+    }
+}
+
+//initialize the score variables within the global scope
+let humanScore = 0 , computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(human, computer){
+    human = human.toLowerCase();
+    
+}
