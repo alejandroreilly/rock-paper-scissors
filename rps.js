014c86d983a30a1d26f10a1398a3b7caaa9672dc
Alex Reilly <iamalexreilly@gmail.com>
2024-07-08 15:24:36 -0400

    Edit the JS to update who wins after 5 rounds
    After 5 rounds, one winner will be displayed. If the user enters
    another input, the round will restart and each variable will be
    at 0 again, to create multiple games.

--- rps.js
+++ rps.js
@@ -24,43 +24,60 @@ function getHumanChoice(){
     }
 }
 
-
 let humanScore = 0 , computerScore = 0;
 const message = document.querySelector("div.score");
 const results = document.querySelector("div.results");
 
-function playRound(human, computer){
-    human = human.toLowerCase();
-    //win cases
-    if ((human === "rock" && computer === "scissors") || (human === "scissors" && computer === "paper") || (human === "paper" && computer === "rock")){
-        message.textContent = (`Computer chose ${computer}. You Win! ${human} beats ${computer}`);
-        humanScore++;
-        results.textContent = (`Your Score: ${humanScore} | Computer Score: ${computerScore}`);
-        return;
+function playGame(){
 
-    //lose conditions vvv
-    }else if((computer === "rock" && human === "scissors") || (computer === "scissors" && human === "paper") || (computer === "paper" && human === "rock")){
-        message.textContent = (`Computer chose ${computer}. You Lose! ${computer} beats ${human}`);
-        computerScore++;
+    function playRound(human, computer){
+        human = human.toLowerCase();
+        //win cases
+        if ((human === "rock" && computer === "scissors") || (human === "scissors" && computer === "paper") || (human === "paper" && computer === "rock")){
+            message.textContent = (`Computer chose ${computer}. You Win! ${human} beats ${computer}`);
+            humanScore++;
+            results.textContent = (`Your Score: ${humanScore} | Computer Score: ${computerScore}`);
+            if (humanScore >= 5){
+                message.textContent = "You got 5! YOU WIN!";
+                humanScore = 0, computerScore = 0;
+            }else if(computerScore >= 5){
+                message.textContent = "Computer got 5! YOU LOSE!";
+                humanScore = 0; computerScore = 0;
+            }
+            return;
 
-        results.textContent = (`Your Score: ${humanScore} | Computer Score: ${computerScore}`);
-        return;
-    }else{
-        //tie
-        message.textContent = (`Computer chose ${computer}. It's a Tie! ${human} equals ${computer}!`);
-        return;
+        //lose conditions vvv
+        }else if((computer === "rock" && human === "scissors") || (computer === "scissors" && human === "paper") || (computer === "paper" && human === "rock")){
+            message.textContent = (`Computer chose ${computer}. You Lose! ${computer} beats ${human}`);
+            computerScore++;
+
+            results.textContent = (`Your Score: ${humanScore} | Computer Score: ${computerScore}`);
+            if (humanScore >= 5){
+                message.textContent = "You got 5! YOU WIN!";
+                humanScore = 0, computerScore = 0;
+            }else if(computerScore >= 5){
+                message.textContent = "Computer got 5! YOU LOSE!";
+                humanScore = 0; computerScore = 0;
+            }
+            return;
+        }else{
+            //tie
+            message.textContent = (`Computer chose ${computer}. It's a Tie! ${human} equals ${computer}!`);
+            return;
+        }
     }
+
+    
+    //add the buttons to js
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', function(){
+            let comp = computerChoice()
+            playRound(button.id, comp)
+        })
+    });
 }
 
-//add the buttons to js
-const buttons = document.querySelectorAll("button");
+playGame();
 
-buttons.forEach((button) => {
-    button.addEventListener('click', function(){
-        let comp = computerChoice()
-        playRound(button.id, comp)
-    })
-});
-
-
-console.log(`Your Score: ${humanScore}. Computer's Score: ${computerScore}`);
